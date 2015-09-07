83fd70f9fbabe00d6fac4c50612b188ccb27eccc
xingzhengfan <fanxingzheng@163.com>
2015-09-07 16:44:19 +0800

    Initial Commit

---
+++ Effective_OC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Effective_OC
+//
+//  Created by 范兴政 on 15/9/7.
+//  Copyright (c) 2015年 fanxingzheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Effective_OC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Effective_OC
+//
+//  Created by 范兴政 on 15/9/7.
+//  Copyright (c) 2015年 fanxingzheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
